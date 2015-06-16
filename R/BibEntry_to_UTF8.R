1d99b14e558b43b8221f9541ddbbc1b661ddbbfb
Pakillo <f.rodriguez.sanc@gmail.com>
2015-06-16 13:17:11 +0200

    added functions to rewrite references in UTF8

---
+++ R/BibEntry_to_UTF8.R
@@ -0,0 +1,33 @@
+#' Encode author names in UTF-8.
+#'
+#' Encode author names in BibEntry objects as UTF-8. Specially useful when working in Windows systems that do not support UTF-8.
+#'
+#' @import RefManageR
+#' @param refs A BibEntry object.
+#' @export
+#' @return A BibEntry object.
+#' @examples \dontrun{
+#' library(knitcitations)
+#' cleanbib()
+#' cite_options(citation_format = "pandoc")
+#' #citet("10.1111/nph.12929") # doesn't work
+#' citep("10.1016/j.tree.2006.09.010")
+#' citet("10.1111/j.1461-0248.2007.01060.x")
+#' ref <- knitcitations:::get_bib()
+#' ref.utf8 <- BibEntry_to_UTF8(ref)
+#'
+#'}
+
+
+BibEntry_to_UTF8 <- function(refs){
+
+  for (i in 1:length(refs)){
+    authors <- paste(refs[[i]]$author, collapse = " and ")
+    refs[[i]]$author <- iconv(authors, to = "UTF-8")
+  }
+
+  refs
+
+}
+
+
---
+++ R/write_bibtex_windows.R
@@ -0,0 +1,43 @@
+#' Encode authors names in UTF-8 and write BibTeX file.
+#'
+#' Modified from \code{\link[knitcitations]{write.bibtex}} to overcome problems with non-UTF-8 encoding in Windows systems.
+#'
+#' @param entry a 'bibentry' object or list of bibentry objects.
+#'        If NULL, writes all that have currently been cited.
+#' @param file output bibtex file. Will automatically append '.bib' if not
+#'  added. if 'NULL' will use stdout.
+#' @param append a logical indicating that bibtex entries be added the the
+#'  file.  If FALSE (default), the file is overwritten.
+#' @param ... additional arguments to WriteBib
+#' @return a list of citation information, invisibly
+#' @import RefManageR
+#' @examples
+#'  write_bibtex_windows(c(citation("knitr"),
+#'                 citation("knitcitations"),
+#'                 citation("RCurl")))
+#' @export
+#' @author Carl Boettiger, slightly modified by F. Rodriguez-Sanchez.
+
+write_bibtex_windows <- function(entry = NULL,
+                         file = "references.bib",
+                         append = FALSE,
+                         ...){
+
+  if(is.null(entry)){
+    entry <- get_bib()
+
+  } else if(is(entry, "bibentry")){
+    entry <- as.BibEntry(entry)
+
+  } else {
+    stop(paste("entry object of class", class(entry), "not recognized"))
+
+  }
+
+  entry.utf8 <- BibEntry_to_UTF8(entry)
+
+  WriteBib(entry.utf8, file=file, append=append, ...)
+}
+
+
+
